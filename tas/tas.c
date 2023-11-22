d4ed2ac59c26837fac08562fdc0908a6d2e3fe3b
mengxiao <limengxiao57@gmail.com>
2023-11-22 23:36:48 +0100

    .

--- tas/tas.c
+++ tas/tas.c
@@ -4,18 +4,12 @@
 
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "tas.h"
 minHp HeapInit(int capacity) {
     minHp newHeap = (minHp)malloc(sizeof(heap));
-    if (newHeap == NULL) {
-        return NULL;
-    }
 
     newHeap->data = (HeapDataType*)malloc(capacity * sizeof(HeapDataType));
-    if (newHeap->data == NULL) {
-        free(newHeap);
-        return NULL;
-    }
 
     newHeap->size = 0;
     newHeap->capacity = capacity;
@@ -29,11 +23,57 @@ void freeMinHeap(minHp minHp) {
     }
 }
 minHp SupprMin(minHp minHp){
+    if(minHp->size==0){
+        return minHp;
+    }
 
+    int parent;
+    parent = 1;
+    while (2 * parent <= minHp->size) {
+        int leftChild = 2 * parent;
+        int rightChild = 2 * parent + 1;
+        int smallerChild = leftChild;
+
+        if (rightChild <= minHp->size && minHp->data[rightChild] < minHp->data[leftChild]) {
+            smallerChild = rightChild;
+        }
+
+        if (minHp->data[parent] <= minHp->data[smallerChild]) {
+            break;
+        }
+
+        HeapDataType temp = minHp->data[parent];
+        minHp->data[parent] = minHp->data[smallerChild];
+        minHp->data[smallerChild] = temp;
+
+        parent = smallerChild;
+    }
+
+    return minHp;
 }
+
 minHp Ajout(minHp minHp,int cle){
-
+    if(minHp->size==minHp->capacity){
+        minHp->capacity++;
+    }
+    minHp->size++;
+    int i = minHp->size;
+    while (i > 1 && cle < minHp->data[i / 2]) {
+        minHp->data[i] = minHp->data[i / 2];
+        i /= 2;
+    }
+    minHp->data[i]=cle;
+    return minHp;
 }
+
 minHp AjoutsIteratifs(minHp minHp,int cles[]){
+    int length = sizeof(cles) / sizeof(cles[0]);
+    //上面不安全，需要改
+    int i=0;
+    while (i<length){
+        Ajout(&minHp,cles[i]);
+        i++;
+    }
+    return minHp;
 
 }
