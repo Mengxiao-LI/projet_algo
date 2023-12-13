350b7d70a132b1f6f308ddf3cb9c8be6d76bf1ed
zhengdao <316321454@qq.com>
2023-12-13 03:59:49 +0100

    .

--- fileBinomiale/filebinomiale.h
+++ fileBinomiale/filebinomiale.h
@@ -5,9 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "../tas/list.h"
-#include "../tas/tas.h"
 #include "../echauffement/echauffement.h"
+
 
 
 //struct arbre binomiale node
@@ -31,24 +30,34 @@ typedef struct FileBinomiale{
 }FileBinomiale;
 
 //primitive tournois
-bool EstVide_T(Tournoi t);
-int Degre(Tournoi t);
-Tournoi Union2Tid(Tournoi t1,Tournoi t2);
-FileBinomiale File(Tournoi t);
+bool EstVide_T(Tournoi* t);
+int Degre(Tournoi* t);
+Tournoi* Union2Tid(Tournoi* t1,Tournoi* t2);
+
 
 //primitive file binomiale
-bool EstVide_FB(FileBinomiale fb);
-Tournoi* MinDeg(FileBinomiale fb);
-FileBinomiale Reste(FileBinomiale fb);
-FileBinomiale AjoutMin(Tournoi t,FileBinomiale fb);
-FileBinomiale Ajout_FB(Tournoi t,FileBinomiale fb);
-FileBinomiale UnionFile(FileBinomiale fb1,FileBinomiale fb2);
-FileBinomiale UFret(FileBinomiale fb1,FileBinomiale fb2,Tournoi* t);
-FileBinomiale SupprMin_FB(FileBinomiale fb);
-FileBinomiale Construction_FB(Tournoi* list_tournois,int size);
+FileBinomiale* File(Tournoi* t);
+bool EstVide_FB(FileBinomiale* fb);
+
+//Renvoie le tournoi de degre minimal dans la file
+Tournoi* MinDeg(FileBinomiale* fb);
+
+FileBinomiale* Reste(FileBinomiale* fb);
+FileBinomiale* AjoutMin(Tournoi* t,FileBinomiale* fb);
+FileBinomiale* Ajout_FB(Tournoi* t,FileBinomiale* fb);
+FileBinomiale* UnionFile(FileBinomiale* fb1,FileBinomiale* fb2);
+FileBinomiale* UFret(FileBinomiale* fb1,FileBinomiale* fb2,Tournoi* t);
+FileBinomiale* SupprMin_FB(FileBinomiale* fb);
+FileBinomiale* Construction_FB(Tournoi** list_tournois,int size);
 void freeArbreBinomialeNode(ArbreBinomialeNode *node);
 void freeTournoi(Tournoi *t);
-void freeFileBinomiale(FileBinomiale fb);
+void freeFileBinomiale(FileBinomiale* fb);
 
+//primitive supplementaire-copy
+ArbreBinomialeNode* copyArbreBinomialeNode(ArbreBinomialeNode* node);
+Tournoi* copyTournoi(Tournoi* tournoi);
 
+//primitive supplementaire-print
+void printFileBinomiale(FileBinomiale* fb);
+void printArbreBinomialeNode(ArbreBinomialeNode *node);
 #endif
