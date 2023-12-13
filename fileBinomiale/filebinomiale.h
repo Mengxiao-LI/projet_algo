efe518ea894b451e23a145f00c15ed7bbacfbada
zhengdao <316321454@qq.com>
2023-12-13 23:56:03 +0100

    .

--- fileBinomiale/filebinomiale.h
+++ fileBinomiale/filebinomiale.h
@@ -33,14 +33,11 @@ typedef struct FileBinomiale{
 bool EstVide_T(Tournoi* t);
 int Degre(Tournoi* t);
 Tournoi* Union2Tid(Tournoi* t1,Tournoi* t2);
-
-
+//Renvoie le tournoi de degre minimal dans la file
+Tournoi* MinDeg(FileBinomiale* fb);
 //primitive file binomiale
 FileBinomiale* File(Tournoi* t);
 bool EstVide_FB(FileBinomiale* fb);
-
-//Renvoie le tournoi de degre minimal dans la file
-Tournoi* MinDeg(FileBinomiale* fb);
 
 FileBinomiale* Reste(FileBinomiale* fb);
 FileBinomiale* AjoutMin(Tournoi* t,FileBinomiale* fb);
@@ -52,11 +49,9 @@ FileBinomiale* Construction_FB(Tournoi** list_tournois,int size);
 void freeArbreBinomialeNode(ArbreBinomialeNode *node);
 void freeTournoi(Tournoi *t);
 void freeFileBinomiale(FileBinomiale* fb);
-
 //primitive supplementaire-copy
 ArbreBinomialeNode* copyArbreBinomialeNode(ArbreBinomialeNode* node);
 Tournoi* copyTournoi(Tournoi* tournoi);
-
 //primitive supplementaire-print
 void printFileBinomiale(FileBinomiale* fb);
 void printArbreBinomialeNode(ArbreBinomialeNode *node);
--- main_Q6.c
+++ main_Q6.c
@@ -38,6 +38,7 @@ int main() {
 //Q2
     printf("-----------Q6.2-----------\n");
 //将上面的liste mode -md5-key128
+//查看是否有编译不一样的词
     Key128* array=convertToKey128Array(listMots, totalWords);
     bool hasCollision=false;
     int j=0;
@@ -48,6 +49,20 @@ int main() {
             hasCollision=true;
         }
         //printf("%08x %08x %08x %08x\n", array[i].part1, array[i].part2, array[i].part3, array[i].part4);
+
+    }
+//查看是否有重复md5的词
+    ABR *tree1 = NULL;
+
+    int cpt=0;
+    for (int i = 0; i < totalWords; i++) {
+        if(!estDans(tree1,array[i])){
+            tree1 = InsertNode(tree1, array[i]);
+        }else{
+            cpt++;
+            printf("Key%d: part1:%08x + part2:%08x + part3:%08x + part4:%08x\n",j, array[i].part1, array[i].part2, array[i].part3, array[i].part4);
+            hasCollision=true;
+        }
 
     }
     if(!hasCollision){
